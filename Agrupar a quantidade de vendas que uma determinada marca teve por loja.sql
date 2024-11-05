5f912997f80e2a15d8bcb6ffe6fbad5798c5d36f
rmarianoti-pg <rmarianoti@gmail.com>
2024-11-05 08:15:45 -0300

    Update Agrupar a quantidade de vendas que uma determinada marca teve por loja.sql

--- Agrupar a quantidade de vendas que uma determinada marca teve por loja.sql
+++ Agrupar a quantidade de vendas que uma determinada marca teve por loja.sql
@@ -1,7 +1,7 @@
 SELECT b.brand_name, st.store_name, COUNT(oi.item_id) AS total_sales
 FROM order_items oi
-JOIN products p ON oi.product_id = p.product_id
-JOIN brands b ON p.brand_id = b.brand_id
-JOIN orders o ON oi.order_id = o.order_id
-JOIN stores st ON o.store_id = st.store_id
+INNER JOIN products p ON oi.product_id = p.product_id
+INNER JOIN brands b ON p.brand_id = b.brand_id
+INNER JOIN orders o ON oi.order_id = o.order_id
+INNER JOIN stores st ON o.store_id = st.store_id
 GROUP BY b.brand_name, st.store_name;
